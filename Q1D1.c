d40ff87f68b0eb6bc205c4a4ba2dd2f759ea3ed5
shubh1207-code <shubh.35209@stu.upes.ac.in>
2026-08-10 23:08:59 +0530

    Update Q1D1.c

--- Q1D1.c
+++ Q1D1.c
@@ -15,12 +15,13 @@ int add(int a, int b)
 {
     return a + b;
 }
-
 int main()
 {
     int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("Enter another number: ");
+    scanf("%d", &b);
     printf("Sum: %d\n", add(a, b));
     return 0;
 }
